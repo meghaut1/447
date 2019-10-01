15ae723af798704c25de7097dbe26e5f2d7c043e
KennyMcD <37344028+KennyMcD@users.noreply.github.com>
2019-10-01 14:36:34 -0400

    SQL db demo in Node.js

---
+++ sqlJsDemo/SQLjs.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "callcenter"
+});
+//con.query("SELECT * FROM event");
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM event", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+.exit
