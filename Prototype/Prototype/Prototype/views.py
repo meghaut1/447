9a5464c6ea2256fc9d14a03a787993a38d65c0d9
rickyt314 <rickyt314@gmail.com>
2019-10-09 00:00:13 -0400

    Prototype update
    Added the ability to fetch submitted values from HTML page

--- Prototype/Prototype/Prototype/views.py
+++ Prototype/Prototype/Prototype/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template, g
+from flask import render_template, g, request
 from Prototype import app
 import sqlite3
 
@@ -20,7 +20,18 @@ def get_db():
 @app.route('/callCenter')
 def callCenter():
     db = get_db() # test to connect to database. Error if it doesn't
+    db.close()
     return render_template("callCenter.html")
+
+# gets input from HTML page
+@app.route('/', methods=['POST'])
+def getInfo():
+    address = request.form['address']
+    phoneNumber = request.form['phoneNumber']
+    date = request.form['timestamp']
+    urgency = request.form['Urgency']
+    time = datetime.now() # current date and time of submission
+    return callCenter()
 
 @app.route('/incidentPanel')
 def incidentPanel():
