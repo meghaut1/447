e4543e4fcbaa023fb708a374f1c4bc08a5ee7ea7
KennyMcD <37344028+KennyMcD@users.noreply.github.com>
2019-12-11 15:21:25 -0500

    bug smasher

--- Prototype/Prototype/Prototype/views.py
+++ Prototype/Prototype/Prototype/views.py
@@ -728,8 +728,9 @@ def deleteMission(missionID):
     conn = sqlite3.connect("callCenter.db")
     cur = conn.cursor()
 
+    cur.execute('UPDATE Volunteer SET = NULL WHERE missionID = ?', (missionID))
     cur.execute('DELETE FROM Mission WHERE missionID = ?', (missionID))
-
+    
     conn.commit()
     cur.close()
     conn.close()
